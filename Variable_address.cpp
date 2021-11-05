66ccd54942b423bc3fd9eeee90fef6383408b506
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-05 11:08:17 +0700

    Create Variable_address.cpp

---
+++ Variable_address.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number1 = 30;
+    int number2 = 85;
+    int number3 = 105;
+
+    cout << "1: "<< &number1 << endl;
+    cout << "2: " << &number2 << endl;
+    cout << "3: " << &number3 << endl;
+}
