91f377daec86226a20d0c745fbc68615df66e8ba
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-05 10:20:54 +0700

    Create if statement.cpp

---
+++ if statement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter an number: ";
+    cin >> number;
+    if (number > 0) {
+        cout << "positive integer: " << number << endl;
+    }
+    return 0;
+}
